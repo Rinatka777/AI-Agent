07904bc42e41f1e13beac03e6a76f387eabc5fe2
Ekaterina Sharifullina <rinatsharifullin@gmail.com>
2025-06-28 16:38:36 +0300

    get file content

---
+++ functions/get_file_content.py
@@ -0,0 +1,20 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    try:
+        target_path = os.path.abspath(os.path.join(working_directory, file_path))
+        working_path = os.path.abspath(working_directory)
+
+        if os.path.commonpath([target_path, working_path]) != working_path:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target_path):
+            return f'Error: "{file_path}" is not a file'
+        
+        MAX_CHARS = 10000
+
+        with open(file_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+    
+    except Exception as e:
+        return f"Error: {str(e)}"
+
