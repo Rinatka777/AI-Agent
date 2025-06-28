5bc060425edbef9516e65cc284d8f30ede189ec7
Ekaterina Sharifullina <rinatsharifullin@gmail.com>
2025-06-28 17:29:10 +0300

    get_file_content+ dummy text + tests

--- tests.py
+++ tests.py
@@ -1,21 +1,20 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def run_tests():
-    print("Test 1: get_files_info('calculator', '.')")
-    print(get_files_info("calculator", "."))
+    print("Test 1")
+    print(get_file_content("calculator", "lorem.txt"))
+    print("\n" + "="*60 + "\n")
+    print("Test 2")
+    print(get_file_content("calculator", "main.py"))
+    print("\n" + "="*60 + "\n")
+    print("Test 3")
+    print(get_file_content("calculator", "pkg/calculator.py"))  
+    print("\n" + "="*60 + "\n")
+    print("Test 4")
+    print(get_file_content("calculator", "/bin/cat"))
     print("\n" + "="*60 + "\n")
 
-    print("Test 2: get_files_info('calculator', 'pkg')")
-    print(get_files_info("calculator", "pkg"))
-    print("\n" + "="*60 + "\n")
-
-    print("Test 3: get_files_info('calculator', '/bin')  # Should error (outside working directory)")
-    print(get_files_info("calculator", "/bin"))
-    print("\n" + "="*60 + "\n")
-
-    print("Test 4: get_files_info('calculator', '../')  # Should error (outside working directory)")
-    print(get_files_info("calculator", "../"))
-    print("\n" + "="*60 + "\n")
 
 if __name__ == "__main__":
     run_tests()
