d2077d9c68a497a93220f8b01b5dbb53c8d4159b
Ekaterina Sharifullina <rinatsharifullin@gmail.com>
2025-07-04 21:30:21 +0300

    Function Declaration started

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google.genai import types
 from google import genai
+from functions.get_files_info import schema_get_files_info
 
 
 def main():
@@ -37,10 +38,14 @@ def main():
 
 
 def generate_content(client, messages, verbose):
+    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config = types.GenerateContentConfig(system_instruction=system_prompt),
     )
+
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
