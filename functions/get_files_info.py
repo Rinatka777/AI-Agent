b287130a227e5e2683ec0e0d4799d79f22b78887
Ekaterina Sharifullina <rinatsharifullin@gmail.com>
2025-07-04 22:38:02 +0300

    finished wiring Function Declaration

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,6 @@
 import os
+from google.genai import types
+
 
 def get_files_info(working_directory, directory=None):
     try:
@@ -33,7 +35,7 @@ def get_files_info(working_directory, directory=None):
     except Exception as e:
         return f"Error: {str(e)}"
     
-    
+
 schema_get_files_info = types.FunctionDeclaration(
     name="get_files_info",
     description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
--- main.py
+++ main.py
@@ -3,7 +3,26 @@ import sys
 from dotenv import load_dotenv
 from google.genai import types
 from google import genai
-from functions.get_files_info import schema_get_files_info
+from functions.get_files_info import schema_get_files_info, get_files_info
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+
+WORKING_DIRECTORY = "calculator"
 
 
 def main():
@@ -38,19 +57,30 @@ def main():
 
 
 def generate_content(client, messages, verbose):
-    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config = types.GenerateContentConfig(system_instruction=system_prompt),
+        config = types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+        ),
     )
 
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
+            if function_call_part.name == "get_files_info":
+                directory = function_call_part.args.get("directory")
+                result = get_files_info(WORKING_DIRECTORY, directory)
+                print(result)
+    else:
+        print("Response:")
+        print(response.text)
 
 
 if __name__ == "__main__":
