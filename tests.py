fea7b6dd7d16097d40d4a2b3202013d44ee8d10c
Ekaterina Sharifullina <rinatsharifullin@gmail.com>
2025-07-08 16:35:24 +0300

    wrote wrong imports in main, now corrected

--- tests.py
+++ tests.py
@@ -1,17 +1,20 @@
 from functions.run_python import run_python_file
+from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+from functions.get_files_info import get_files_info
 
 def run_tests():
     print("Test 1")
-    print(run_python_file("calculator", "main.py"))
+    print(get_file_content({'file_path': 'main.py'}))
     print("\n" + "="*60 + "\n")
     print("Test 2")
-    print(run_python_file("calculator", "tests.py"))
+    print(write_file({'file_path': 'main.txt', 'content': 'hello'}))
     print("\n" + "="*60 + "\n")
     print("Test 3")
-    print(run_python_file("calculator", "../main.py"))
+    print(run_python_file({'file_path': 'main.py'}))
     print("\n" + "="*60 + "\n")
     print("Test 4")
-    print(run_python_file("calculator", "nonexistent.py"))
+    print(get_files_info({'directory': 'pkg'}))
     print("\n" + "="*60 + "\n")
 
 if __name__ == "__main__":
